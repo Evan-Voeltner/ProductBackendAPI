a85a2f51c3178a940d2f5f762b0a4de94c5417ee
Evan Voeltner <evanvoeltner@gmail.com>
2022-09-26 09:58:44 -0500

    Created the GET and POST methods

--- products/views.py
+++ products/views.py
@@ -1,8 +1,41 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ProductSerializer
+from .models import Product
 
 
 @api_view(['GET'])
 def products_list(request):
 
+
     return Response('ok')
+
+@api_view(['GET', 'POST'])
+def products_list(request):
+    
+    if request.method == 'GET':
+        cars = Product.objects.all()
+        serializer = ProductSerializer(cars, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer =  ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def car_detail(request, pk):
+#     car = get_object_or_404(Car, pk=pk)
+#     if request.method == 'GET':
+#         serializer = CarSerializer(car)
+#         return Response(serializer.data)
+#     elif request.method == 'PUT':
+#         serializer = CarSerializer(car, data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+#     elif request.method == 'DELETE':
+#         car.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
