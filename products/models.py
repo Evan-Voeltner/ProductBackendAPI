4290c840a9e49d4c0b8b1e8d0d0afc3f0ac59f7e
Evan Voeltner <evanvoeltner@gmail.com>
2022-09-26 09:16:57 -0500

    Created a models child called products with all the necesarry fields

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
+from turtle import title
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    price = models.IntegerField()
+    inventory_quantity = models.IntegerField()
